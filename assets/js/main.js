fee37d621a49535757661cd476e2475b2ea759ba
titohajili <maqatoto963@gmail.com>
2024-03-13 15:57:50 +0400

    Header & Nav

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*========= Show Menu ==========*/
+const navClose = document.getElementById('nav-close')
+const navToggle = document.getElementById('nav-toggle')
+const navMenu = document.getElementById('nav-menu')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*========== REMOVE MENU MOBILE ===========*/
+
+    const navLink = document.querySelectorAll('.nav__link')
+
+    const linkAction = () => {
+        const navMenu = document.getElementById('nav-menu')
+
+        navMenu.classList.remove('show-menu')
+    
+    
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
