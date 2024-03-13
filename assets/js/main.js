519dba4b47023f0aae48c9608d9f792a7bb14444
titohajili <maqatoto963@gmail.com>
2024-03-13 16:59:59 +0400

    ADD BLUR TO HEADER

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,12 @@ if(navClose){
     
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*============ ADD BLUR TO HEADER ===========*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
