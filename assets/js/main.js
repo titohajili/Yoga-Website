fd2bda7368b44af4e9e81dfb8b0f15ed28deb326
titohajili <maqatoto963@gmail.com>
2024-03-15 19:05:01 +0400

    Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,36 @@ const blurHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('blur-header')
                        : header.classList.remove('blur-header')
 }
-window.addEventListener('scroll', blurHeader)
+window.addEventListener('scroll', blurHeader);
+
+/*========== Scroll Up ==========*/
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
+
+/*========== Scroll Sections Link =========*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                sectionClass.classList.add('active-link');
+            } else {
+                sectionClass.classList.remove('active-link');
+            }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
